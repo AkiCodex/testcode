ecd9cf2c16d1156a6bb2205c40edab3eb7f10aae
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-04-11 12:57:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def print_even_numbers(lst):
+    for num in lst:
+        if num % 2 == 0:
+            print(num)
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print_even_numbers(my_list)
